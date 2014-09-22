2cfd292b4ae5f314c6abb65b9eba4196b6fc7a21
famalone <finmalone@gmail.com>
2014-09-22 11:25:18 -0500

    Target Completed

--- Chapter 3/Target.java
+++ Chapter 3/Target.java
@@ -1,34 +1,44 @@
 import java.awt.Graphics2D;
-import java.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.Color;
 public class Target
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
+    private int xLeft;
+    private int yTop;
 
     /**
      * Default constructor for objects of class Target
      */
-    public Target()
+    public Target(int x,int y)
     {
-        // initialise instance variables
-        x = 0;
+        this.xLeft = x;
+        this.yTop = y;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
+    public void draw(Graphics2D g2)
     {
-        // put your code here
-        return x+y;
+        Ellipse2D.Double redC1 = new Ellipse2D.Double(xLeft - 50,yTop - 50,100,100);
+        Ellipse2D.Double whiteC1 = new Ellipse2D.Double(xLeft - 40,yTop - 40,80,80);
+        Ellipse2D.Double redC2 = new Ellipse2D.Double(xLeft - 30,yTop - 30,60,60);
+        Ellipse2D.Double whiteC2 = new Ellipse2D.Double(xLeft - 20,yTop - 20,40,40);
+        Ellipse2D.Double redC3 = new Ellipse2D.Double(xLeft - 10,yTop - 10,20,20);
+        
+        g2.draw(redC1);
+        g2.draw(whiteC1);
+        g2.draw(redC2);
+        g2.draw(whiteC2);
+        g2.draw(redC3);
+        
+        g2.setColor(Color.red);
+        g2.fill(redC1);
+        g2.setColor(Color.white);
+        g2.fill(whiteC1);
+        g2.setColor(Color.red);
+        g2.fill(redC2);
+        g2.setColor(Color.white);
+        g2.fill(whiteC2);
+        g2.setColor(Color.red);
+        g2.fill(redC3);
     }
 
 }
--- Chapter 3/TargetViewer.java
+++ Chapter 3/TargetViewer.java
@@ -6,7 +6,7 @@ public class TargetViewer
     {
        JFrame frame = new JFrame();
        
-       frame.setSize(400,400);
+       frame.setSize(400,450);
        frame.setTitle("Target");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        
