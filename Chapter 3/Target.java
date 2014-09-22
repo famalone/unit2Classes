9093829abf066be32a176531fe26bbdd16ff6bf4
famalone <finmalone@gmail.com>
2014-09-22 10:35:58 -0500

    Target

---
+++ Chapter 3/Target.java
@@ -0,0 +1,34 @@
+import java.awt.Graphics2D;
+import java.geom.Ellipse2D;
+public class Target
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private int x;
+
+    /**
+     * Default constructor for objects of class Target
+     */
+    public Target()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
---
+++ Chapter 3/TargetComponent.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+/**
+ * Draws a target.
+ */
+public class TargetComponent extends JComponent
+{
+    public void paintComponent(Graphics g)
+    {
+       Graphics2D g2 = (Graphics2D) g;
+       
+       Target target = new Target(200,200);
+       
+    }
+
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
